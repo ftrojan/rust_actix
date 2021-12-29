130cdfc082aa83d197cd6252c75449f76b6860f8
filip_trojan <Tarantula2018>
2021-12-29 18:59:34 +0100

    improved logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use log::debug;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result, HttpRequest};
 use serde::{Serialize, Deserialize};
+use std::fmt;
 
 struct AppState {
     foo: String,
@@ -10,20 +12,34 @@ struct MyObj {
     name: &'static str,
 }
 
+impl fmt::Display for MyObj {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "name: {}", self.name)
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct MyParams {
     name: String,
 }
 
+impl fmt::Display for MyParams {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "name: {}", self.name)
+    }
+}
+
 #[get("/json")]
 async fn json() -> impl Responder {
     let x = MyObj { name: "user" };
+    debug!("json: {}", x);
     let body = serde_json::to_string(&x).unwrap();
     HttpResponse::Ok().content_type("application/json").body(body)
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    debug!("hello");
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -33,16 +49,19 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 async fn manual_hello() -> impl Responder {
+    debug!("hey");
     HttpResponse::Ok().body("Hey there!")
 }
 
 #[get("/form")]
 async fn form() -> impl Responder {
+    debug!("Rendering form");
     HttpResponse::Ok().body(include_str!("../static/form.html"))
 }
 
 #[post("/post1")]
 async fn handle_post_1(params: web::Form<MyParams>) -> Result<HttpResponse> {
+    debug!("params: {}", params);
     Ok(HttpResponse::Ok()
         .content_type("text/plain")
         .body(format!("Your name is {}", params.name)))
@@ -54,6 +73,7 @@ async fn handle_post_2(
     state: web::Data<AppState>,
     params: web::Form<MyParams>,
 ) -> HttpResponse {
+    debug!("params: {}, AppState.foo: {}", params, state.foo);
     HttpResponse::Ok().content_type("text/plain").body(format!(
         "Your name is {}, and in AppState I have foo: {}",
         params.name, state.foo
@@ -63,8 +83,8 @@ async fn handle_post_2(
 /// Request and POST Params
 #[post("/post3")]
 async fn handle_post_3(req: HttpRequest, params: web::Form<MyParams>) -> impl Responder {
-    println!("Handling POST request: {:?}", req);
-
+    debug!("Handling POST request: {:?}", req);
+    debug!("POST parameters: {:?}", params.name);
     HttpResponse::Ok()
         .content_type("text/plain")
         .body(format!("Your name is {}", params.name))
@@ -87,6 +107,8 @@ fn app_config(config: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+    debug!("started");
     HttpServer::new(|| {
         App::new()
             .configure(app_config)
